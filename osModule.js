eff1b6a864c219b9c860ef9a8bf209c5454d70cb
María Delgado <mariad1996@gmail.com>
2024-01-10 12:40:00 +0100

    osModule finished

--- osModule.js
+++ osModule.js
@@ -1,10 +1,17 @@
-// Ejemplo de output:
-// Nombre: darwin
-// Tipo: Darwin
-// Versión: Darwin Kernel Version 23.1.0: Mon Oct  9 21:27:27 PDT 2023; root:xnu-10002.41.9~6/RELEASE_X86_64
-// Arquitectura: x64
-// CPUs: 12
-// MemoriaTotal: 16384.00 MB
-// MemoriaLibre: 370.71 MB
-
 // os: version(), platform(), cpus(), totalmem(), networkInterfaces, freemem(), type(), arch()
+
+// node osModule.js
+const os = require('node:os')
+
+const osOutput = {
+    name: os.platform(),
+    type: os.type(),
+    version: os.version(),
+    arch: os.arch(),
+    cpus: os.cpus().length,
+    totalmem: `${(os.totalmem() * Math.pow(10, -6)).toFixed(2)} MB`,
+    freemem: `${(os.freemem() * Math.pow(10, -6)).toFixed(2)} MB`
+}
+
+console.log(osOutput)
+module.exports = osOutput
